22ee2847adca11565c817374d72b07242082b1f6
mdnishat27 <59754386+mdnishat27@users.noreply.github.com>
2020-10-01 22:23:04 +0600

    home page design

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,64 @@
 import React from 'react';
-import {Text, Image, StyleSheet, View} from 'react-native';
+import {Text, Image, StyleSheet} from 'react-native';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import MyButton from '../components/MyButton';
 
 
 const HomeScreen= () =>{
     return(
-        <View>
-            <Text style={style.textstyle}>Hello world</Text>
-            <Image source={require('../../assets/IUTlogo.png')}/>
-        </View>
+        <ScrollView contentContainerStyle={style.viewstyle}>
+            <Image style={style.imagestyle} source={require('../../assets/IUTlogo.png')}/>
+            <Text style={style.textstyle}>Department of</Text>
+            <Text style={style.text2style}>Computer Science and Engineering</Text>
+            <Text style={style.textstyle}>Programme : SWE</Text>
+            <Text>{"\n\n"}</Text>
+            <TouchableOpacity onPress={function(){
+                console.log("My profile pressed");
+            }}>
+                <Text style={style.profiletextstyle}>My Profile</Text>
+            </TouchableOpacity>
+            <Text>{"\n"}</Text>
+
+            <MyButton title="Semester wise Course List" onPress={function(){
+                console.log("course list button pressed");
+            }}/>
+            <MyButton title="List of Faculty Members" onPress={function(){
+                console.log("faculty list button pressed");
+            }}/>
+        </ScrollView>
     );
 }
 
 const style = StyleSheet.create(
     {
-       textstyle:{
-           
-       } 
+        viewstyle:{
+            flexGrow:1,
+            alignItems:'center',
+            justifyContent: 'space-around',
+            borderRadius:10,
+            borderBottomWidth:20,
+            borderLeftWidth:10,
+            borderRightWidth:10,
+            borderTopWidth:10,
+            borderColor:'transparent'
+        },
+        textstyle:{
+            color:'black',
+            fontSize:20
+        },
+        text2style:{
+            color:'black',
+            fontSize:20,
+            fontWeight:"bold"
+        },
+        profiletextstyle:{
+            color:'blue',
+            fontSize:20
+        },
+        imagestyle:{
+            height:130,
+            width:80,
+        }
     }
 );
 
